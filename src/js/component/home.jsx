73fe161d11a2e098593071d0c599d2e37bfce895
Sblaise123 <127683538+Sblaise123@users.noreply.github.com>
2023-06-16 04:47:22 +0000

    TodoFEtch2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from 'react';
 const Todo = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+  const [editTodo, setEditTodo] = useState(null);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/')
+    fetch('https://jsonplaceholder.typicode.com/todos')
       .then(response => response.json())
       .then(data => setTodos(data))
       .catch(error => {
@@ -20,7 +21,7 @@ const Todo = () => {
       completed: false
     };
 
-    fetch('https://jsonplaceholder.typicode.com/todos/', {
+    fetch('https://jsonplaceholder.typicode.com/todos', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -45,7 +46,7 @@ const Todo = () => {
       return todo;
     });
 
-    fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+    fetch(`https://jsonplaceholder.typicode.com/todos${todoId}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
@@ -61,15 +62,74 @@ const Todo = () => {
       });
   };
 
+  const handleDeleteTodo = todoId => {
+    fetch(`https://jsonplaceholder.typicode.com/todos${todoId}`, {
+      method: 'DELETE'
+    })
+      .then(() => {
+        const updatedTodos = todos.filter(todo => todo.id !== todoId);
+        setTodos(updatedTodos);
+      })
+      .catch(error => {
+        console.error('Error deleting todo:', error);
+      });
+  };
+
+  const handleEditTodo = todo => {
+    setEditTodo(todo);
+    setNewTodo(todo.title);
+  };
+
+  const handleUpdateTodo = () => {
+    if (!editTodo) return;
+
+    const updatedTodo = {
+      ...editTodo,
+      title: newTodo
+    };
+
+    fetch(`https://jsonplaceholder.typicode.com/todos${editTodo.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedTodo)
+    })
+      .then(response => response.json())
+      .then(data => {
+        const updatedTodos = todos.map(todo => {
+          if (todo.id === editTodo.id) {
+            return data;
+          }
+          return todo;
+        });
+        setTodos(updatedTodos);
+        setNewTodo('');
+        setEditTodo(null);
+      })
+      .catch(error => {
+        console.error('Error updating todo:', error);
+      });
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={event => setNewTodo(event.target.value)}
-      />
-      <button onClick={handleAddTodo}>Add</button>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={event => setNewTodo(event.target.value)}
+        />
+        {editTodo ? (
+          <>
+            <button onClick={handleUpdateTodo}>Update</button>
+            <button onClick={() => setEditTodo(null)}>Cancel</button>
+          </>
+        ) : (
+          <button onClick={handleAddTodo}>Add</button>
+        )}
+      </div>
       <ul>
         {todos.map(todo => (
           <li key={todo.id}>
@@ -78,7 +138,11 @@ const Todo = () => {
               checked={todo.completed}
               onChange={() => handleToggleComplete(todo.id)}
             />
-            {todo.title}
+            <span>{todo.title}</span>
+            <div>
+              <button onClick={() => handleEditTodo(todo)}>Edit</button>
+              <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            </div>
           </li>
         ))}
       </ul>
