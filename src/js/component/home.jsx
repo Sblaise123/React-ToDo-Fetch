1f2ecaf67315d7231d3b054790fc35300f9c1343
Sblaise123 <127683538+Sblaise123@users.noreply.github.com>
2023-06-16 04:19:25 +0000

    TodoFetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+      .then(response => response.json())
+      .then(data => setTodos(data))
+      .catch(error => {
+        console.error('Error fetching todos:', error);
+      });
+  }, []);
+
+  const handleAddTodo = () => {
+    const newTodoItem = {
+      id: Date.now(),
+      title: newTodo,
+      completed: false
+    };
+
+    fetch('https://jsonplaceholder.typicode.com/todos/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newTodoItem)
+    })
+      .then(response => response.json())
+      .then(data => {
+        setTodos([...todos, data]);
+        setNewTodo('');
+      })
+      .catch(error => {
+        console.error('Error adding new todo:', error);
+      });
+  };
+
+  const handleToggleComplete = todoId => {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === todoId) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+
+    fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedTodos.find(todo => todo.id === todoId))
+    })
+      .then(response => response.json())
+      .then(data => {
+        setTodos(updatedTodos);
+      })
+      .catch(error => {
+        console.error('Error updating todo:', error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={event => setNewTodo(event.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleToggleComplete(todo.id)}
+            />
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default Home;
+export default Todo;
